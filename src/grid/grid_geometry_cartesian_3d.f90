e49f1c2478ce605cbfd8914df103701f4cfc1f62
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-09-28 16:54:34 +0100

    DEBUG 6

--- src/grid/grid_geometry_cartesian_3d.f90
+++ src/grid/grid_geometry_cartesian_3d.f90
@@ -1,17 +1,23 @@
 module grid_geometry_specific
 
+  use core_lib
+  use mpi_core
+  use mpi_hdf5_io
+  use type_photon
   use type_grid_cell
+  use type_grid
+  use grid_io
+  use counters
 
   implicit none
   save
 
 contains
 
-  real function cell_width(cell, idir)
+  real(dp) function cell_width(cell, idir)
     implicit none
     type(grid_cell),intent(in) :: cell
     integer,intent(in) :: idir
-    cell_width = 0.
   end function cell_width
 
 end module grid_geometry_specific
