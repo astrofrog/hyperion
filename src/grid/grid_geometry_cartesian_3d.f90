823d85f8b3b48d7f7e9015c448020e4c385b4f57
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-09-28 17:02:30 +0100

    No func

--- src/grid/grid_geometry_cartesian_3d.f90
+++ src/grid/grid_geometry_cartesian_3d.f90
@@ -9,13 +9,7 @@ module grid_geometry_specific
   implicit none
   save
 
-contains
+  type(grid_cell) :: cell
 
-  real(dp) function cell_width(cell, idir)
-    implicit none
-    type(grid_cell),intent(in) :: cell
-    integer,intent(in) :: idir
-    cell_width=0.
-  end function cell_width
 
 end module grid_geometry_specific
