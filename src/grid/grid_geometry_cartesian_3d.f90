c168eba0b89b849677df15eab2995937b81985fc
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-09-28 17:01:15 +0100

    DEBUG 11

--- src/grid/grid_geometry_cartesian_3d.f90
+++ src/grid/grid_geometry_cartesian_3d.f90
@@ -1,6 +1,5 @@
 module grid_geometry_specific
 
-  use core_lib
   use mpi_core
   use type_photon
   use type_grid_cell
@@ -11,7 +10,7 @@ module grid_geometry_specific
 
 contains
 
-  real(dp) function cell_width(cell, idir)
+  real function cell_width(cell, idir)
     implicit none
     type(grid_cell),intent(in) :: cell
     integer,intent(in) :: idir
