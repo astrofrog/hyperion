f9097994448f4a16d26af2688726113059a2e608
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-09-28 17:39:00 +0100

    DEBUG 220

--- src/core/type_cell_id_3d.f90
+++ src/core/type_cell_id_3d.f90
@@ -7,7 +7,7 @@ module type_grid_cell
 
   public :: grid_cell
   type grid_cell
-     integer :: i1, i2, i3, ic
+     integer :: i1
   end type grid_cell
 
   type(grid_cell),parameter,public :: invalid_cell = grid_cell(-1,-1,-1,-1)
@@ -24,7 +24,7 @@ module type_grid_cell
 
   public :: wall_id
   type wall_id
-     integer :: w1=0, w2=0, w3=0
+     integer :: w1=0
   end type wall_id
 
 contains
@@ -33,14 +33,12 @@ contains
     implicit none
     type(wall_id), intent(in) :: a,b
     c%w1 = a%w1 + b%w1
-    c%w2 = a%w2 + b%w2
-    c%w3 = a%w3 + b%w3
   end function add_wall
 
   logical function equal(a,b)
     implicit none
     type(grid_cell), intent(in) :: a,b
-    equal = a%i1 == b%i1 .and. a%i2 == b%i2 .and. a%i3 == b%i3
+    equal = a%i1 == b%i1
   end function equal
 
 end module type_grid_cell
